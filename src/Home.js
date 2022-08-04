bd47a9b0d4596e891b57b33bbfac3d5f388bbd97
Dennis-star1001 <tdennis.developer@gmail.com>
2022-08-04 17:31:38 +0100

    Delete button

--- src/Home.js
+++ src/Home.js
@@ -11,12 +11,18 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum.....', author: 'mario', id: '3' }
     ]);
 
+    const handleDelete = (id) => {
+
+        const newBlogs = blogs.filter((e) => e.id !== id)
+        setBlogs(newBlogs)
+    }
+
 
 
     return (
         <Box className="home">
-            <BlogList blogs={blogs} title="All Blogs" />
-            <BlogList blogs={blogs.filter((e)=> e.author === 'mario')} title="Dennis's Blog" />
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            {/* <BlogList blogs={blogs.filter((e)=> e.author === 'mario')} title="Dennis's Blog" /> */}
         </Box>
     );
 }
